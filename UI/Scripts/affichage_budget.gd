8a21a3bcb6799a0809e584322d3f352d18568ab3
Carl <carl.cochet@gmail.com>
2022-11-17 10:28:36 +0100

    Adapt cost display to value

--- UI/Scripts/affichage_budget.gd
+++ UI/Scripts/affichage_budget.gd
@@ -5,4 +5,15 @@ extends TextureRect
 
 
 func update():
-	cout_total.text = str(GlobalData.equipe_actuelle.calcul_budget())
+	var budget = GlobalData.equipe_actuelle.calcul_budget()
+	cout_total.text = str(budget)
+	
+	if budget <= 6000:
+		cout_total.label_settings.font_color = Color(1.0, 0.753, 0.0)
+	else:
+		cout_total.label_settings.font_color = Color(1.0, 0.0, 0.0)
+	
+	if budget < 10000:
+		cout_total.label_settings.font_size = 22
+	else:
+		cout_total.label_settings.font_size = 18
