8379780cbc1c7ba5e4caf2cb817677961cbf5006
Carl <carl.cochet@gmail.com>
2023-01-21 18:35:57 +0100

    Try to fix disconnect

--- UI/Scripts/multijoueur_setup.gd
+++ UI/Scripts/multijoueur_setup.gd
@@ -7,15 +7,19 @@ var PORT: int = 55001
 @onready var texte_attente: Label = $TexteAttente
 
 
+func _ready():
+	Client.reset()
+	Client.multiplayer_peer.create_server(PORT)
+	Client.multiplayer.multiplayer_peer = Client.multiplayer_peer
+	Client.multiplayer_peer.peer_connected.connect(func(id): Client.ajouter_peer(id))
+
+
 func _input(event):
 	if Input.is_key_pressed(KEY_ESCAPE) and event is InputEventKey and not event.echo:
 		get_tree().change_scene_to_file("res://UI/menu_principal.tscn")
 
 
 func _on_heberger_pressed():
-	Client.multiplayer_peer.create_server(PORT)
-	Client.multiplayer.multiplayer_peer = Client.multiplayer_peer
-	Client.multiplayer_peer.peer_connected.connect(func(id): Client.ajouter_peer(id))
 	Client.is_host = true
 	Client.ajouter_peer(1)
 	menu.visible = false
@@ -23,8 +27,9 @@ func _on_heberger_pressed():
 
 
 func _on_rejoindre_pressed():
-	if Client.peer_count < 2:
-		Client.multiplayer_peer.create_client("ws://" + adresse.text + ":" + str(PORT))
+	Client.reset()
+	var error = Client.multiplayer_peer.create_client("ws://" + adresse.text + ":" + str(PORT))
+	if error == OK:
 		Client.multiplayer.multiplayer_peer = Client.multiplayer_peer
 		get_tree().change_scene_to_file("res://UI/choix_equipe_multi.tscn")
 
