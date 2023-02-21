77959ff2dfdcf5125f6dab967e11e549ff1f89c6
Carl <carl.cochet@gmail.com>
2023-02-21 20:34:23 +0100

    Add poison and regen display in timeline popup

--- Fight/Scripts/effet.gd
+++ Fight/Scripts/effet.gd
@@ -56,6 +56,8 @@ func _init(p_lanceur, p_cible, p_categorie, p_contenu, p_critique, p_centre, p_a
 	if contenu is Dictionary:
 		duree = trouve_duree(contenu)
 	debuffable = duree > 0
+	if duree > 0 and categorie in ["DOMMAGE_FIXE", "SOIN"]:
+		valeur_dommage = contenu[trouve_crit()]["valeur"]
 
 
 func trouve_duree(data: Dictionary):
