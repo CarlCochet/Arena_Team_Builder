b71ba9a77578f922d2e07cebf4aa037df638cdef
Carl <carl.cochet@gmail.com>
2023-07-13 00:17:31 +0200

    Add RPC calls for some settings changes

--- UI/Scripts/multijoueur_settings.gd
+++ UI/Scripts/multijoueur_settings.gd
@@ -33,12 +33,39 @@ func _on_valider_pressed():
 
 
 func _on_budget_drag_ended(value_changed: float):
-	pass # Replace with function body.
+	rpc("update_limite_budget", int(conditions_values[0].text))
 
 
 func _on_perso_drag_ended(value_changed: float):
-	pass # Replace with function body.
+	rpc("update_limite_budget", int(conditions_values[1].text))
 
 
 func _on_ms_drag_ended(value_changed: float):
-	pass # Replace with function body.
+	rpc("update_limite_budget", int(conditions_values[2].text))
+
+
+@rpc("any_peer")
+func update_limite_budget(budget: int):
+	pass
+
+
+@rpc("any_peer")
+func update_personnages_max(perso_max: int):
+	pass
+
+
+@rpc("any_peer")
+func update_tours_avant_ms(tours: int):
+	pass
+
+
+func _on_budget_value_changed(value: float):
+	conditions_values[0].text = str(int(value))
+
+
+func _on_perso_value_changed(value: float):
+	conditions_values[1].text = str(int(value))
+
+
+func _on_ms_value_changed(value: float):
+	conditions_values[2].text = str(int(value))
